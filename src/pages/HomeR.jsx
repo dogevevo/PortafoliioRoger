3cefc6043b0a20d71aaf214d61bb0b7d9daaf802
dogevevo <rstevenpr@gmail.com>
2025-06-26 16:53:25 -0600

    others sections

--- src/pages/HomeR.jsx
+++ src/pages/HomeR.jsx
@@ -3,6 +3,10 @@ import { ThemeToogle } from '../components/ThemeToogle'
 import { StarBackground } from '../components/StarBackground'
 import { Navbar } from '../components/Navbar'
 import { HeroSection } from '../components/HeroSection'
+import { AboutSection } from '../components/AboutSection'
+import { SkillsSection } from '../components/SkillsSection'
+import { ProjectsSection } from '../components/ProjectsSection'
+import { ContactSection } from '../components/ContactSection'
 
 export const Home = () => {
   return (
@@ -16,6 +20,10 @@ export const Home = () => {
         {/* Main content */}
         <main>
           <HeroSection /> 
+          <AboutSection />
+          <SkillsSection />
+          <ProjectsSection /> 
+          <ContactSection /> 
         </main>
         {/* Footer */}
 
